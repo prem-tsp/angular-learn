df567a4f405860a2b8ed23ba87868f208ccd1afd
prem-tsp <premtsp98@gmail.com>
2019-11-23 12:20:35 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,27 +8,76 @@ import { map, filter } from 'rxjs/operators'
 })
 export class AppComponent  {
   name = 'Angular';
+  pageNo: number
   recordsPerPage: number
   data = [
     {id: 1, name: "Michael Scott"},
     {id: 2, name: "Jim Halpert"},
     {id: 3, name: "Pam Beesly"},
     {id: 4, name: "Dwight K Shrute"},
-    {id: 5, name: "Oscar"}
+    {id: 5, name: "Creed Bratton"}
   ]
   subject = new BehaviorSubject(this.data)
   sub$ = this.subject.asObservable()
+  noOfPages: number = Math.ceil(this.data.length / this.recordsPerPage) 
+  current: any[] = [];
+  
   onSubmit(recordsPerPage: number) {
     this.recordsPerPage = recordsPerPage
+    console.log(this.data.length)
+    console.log(this.noOfPages)
+  }
+
+  changePage(pageNo)
+  {
+    if(this.noOfPages != undefined)
+    {
+    if(pageNo >= this.noOfPages) 
+    {
+      
+      this.pageNo = this.noOfPages
+      pageNo = this.noOfPages
+    }
+    if(pageNo < 1)
+    {
+      this.pageNo = 1
+      pageNo = 1
+       this.subject.pipe(
+        map(val => (val.forEach(e => console.log(e.id)))),
+       )
+    }
+    }
+    else console.log("Wrong Invocation")
+  }
+  d: {id: number, name: string};
+  next()
+  {
+    this.pageNo++
+    console.log(this.pageNo)
+    this.changePage(this.pageNo)
+    console.log(this.pageNo)
+  }
+  prev()
+  {
+    this.pageNo--
+    this.changePage(this.pageNo)
+    console.log(this.pageNo);
   }
   check()
   {
-    this.sub$.subscribe(val => console.log(val));
-    this.subject.next([{id: 6, name:"Oscar"}])
-    console.log(this.recordsPerPage)
+    this.sub$.subscribe(val => val.forEach(e => console.log(e.name)));
+    this.d = {id: 6, name:"Oscar"}
+    this.subject.next([this.d]) 
+    if(this.recordsPerPage != undefined && this.recordsPerPage <= this.data.length)
+      console.log(this.recordsPerPage)
+    else
+      console.log("Enter Valid size")
   }
   constructor()
   {
-
+    this.pageNo = 1;
+    this.data.forEach(obj => {
+      this.current.push(obj.id)
+    })
   }
 }
